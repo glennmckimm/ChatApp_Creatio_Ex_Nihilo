b9bc4c07c0d169cf90e8c2171dce9f4f6833aed7
GLENN BAKER <T7014713@live.tees.ac.uk>
2019-03-02 03:17:09 +0000

    Added backPressed method

--- app/src/main/java/uk/ac/tees/t7014713/exnihilo_chatapplication/MainActivity.java
+++ app/src/main/java/uk/ac/tees/t7014713/exnihilo_chatapplication/MainActivity.java
@@ -1,13 +1,32 @@
 package uk.ac.tees.t7014713.exnihilo_chatapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int TIME_DELAY = 2000;
+    private static long backPressed;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (backPressed + TIME_DELAY > System.currentTimeMillis()) {
+            Intent intent = new Intent(Intent.ACTION_MAIN);
+            intent.addCategory(Intent.CATEGORY_HOME);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+            finish();
+        } else {
+            Toast.makeText(this,"Press again to exit", Toast.LENGTH_SHORT).show();
+        }
+        backPressed = System.currentTimeMillis();
     }
 }
