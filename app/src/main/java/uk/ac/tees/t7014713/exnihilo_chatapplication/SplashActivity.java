b39e0530182550ce8344ad852a44b7225f033d1d
glennmckimm <t7014713@live.tees.ac.uk>
2019-03-02 19:04:45 +0000

    Added basic splash activity and a login activity

---
+++ app/src/main/java/uk/ac/tees/t7014713/exnihilo_chatapplication/SplashActivity.java
@@ -0,0 +1,33 @@
+package uk.ac.tees.t7014713.exnihilo_chatapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Glenn on 02/03/2019.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Thread myThread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    sleep(1500);
+                    Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        myThread.start();
+    }
+}
