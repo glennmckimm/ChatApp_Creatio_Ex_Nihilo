373d0bd730cc06312b1d4320fa57c630819162a9
glennmckimm <t7014713@live.tees.ac.uk>
2019-03-03 17:33:51 +0000

    Added an empty user class/layout and a splash screen

--- app/src/main/java/uk/ac/tees/t7014713/exnihilo_chatapplication/MainActivity.java
+++ app/src/main/java/uk/ac/tees/t7014713/exnihilo_chatapplication/MainActivity.java
@@ -1,10 +1,11 @@
 package uk.ac.tees.t7014713.exnihilo_chatapplication;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
@@ -15,11 +16,20 @@ public class MainActivity extends AppCompatActivity {
 
     private static final int TIME_DELAY = 2000;
     private static long backPressed;
+    private Button btnUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnUser = (Button) findViewById(R.id.btnUser);
+        btnUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                startActivity(new Intent(MainActivity.this, UserActivity.class));
+            }
+        });
     }
 
     @Override
---
+++ app/src/main/java/uk/ac/tees/t7014713/exnihilo_chatapplication/UserActivity.java
@@ -0,0 +1,20 @@
+package uk.ac.tees.t7014713.exnihilo_chatapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Glenn on 03/03/2019.
+ */
+
+public class UserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user);
+
+        getSupportActionBar().setTitle("User");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+}
