eb40027b9a8322272b8a007d61ea2a6a44f5a909
glennmckimm <t7014713@live.tees.ac.uk>
2019-03-04 15:15:00 +0000

    Changed users layout and added two skeleton classes/fragments

---
+++ app/src/main/java/uk/ac/tees/t7014713/exnihilo_chatapplication/SectionsPagerAdapter.java
@@ -0,0 +1,44 @@
+package uk.ac.tees.t7014713.exnihilo_chatapplication;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Glenn on 04/03/2019.
+ */
+
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+    private static final String TAG = "SectionsPagerAdapter";
+
+    private final List<Fragment> fragmentList = new ArrayList<>();
+    private final List<String> fragmentListTitles = new ArrayList<>();
+
+    public void addFragment(Fragment fragment, String title) {
+        fragmentList.add(fragment);
+        fragmentListTitles.add(title);
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return fragmentListTitles.get(position);
+    }
+
+    public SectionsPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return fragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return fragmentListTitles.size();
+    }
+}
--- app/src/main/java/uk/ac/tees/t7014713/exnihilo_chatapplication/UserActivity.java
+++ app/src/main/java/uk/ac/tees/t7014713/exnihilo_chatapplication/UserActivity.java
@@ -1,9 +1,13 @@
 package uk.ac.tees.t7014713.exnihilo_chatapplication;
 
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 
 import com.firebase.ui.auth.AuthUI;
@@ -16,10 +20,32 @@ import com.google.android.gms.tasks.Task;
 
 public class UserActivity extends AppCompatActivity {
 
+    private static final String TAG = "UserActivity";
+
+    private SectionsPagerAdapter mSectionsPagerAdapter;
+    private ViewPager mViewPager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate: UserActivityStarted");
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
+
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+        mViewPager = (ViewPager) findViewById(R.id.viewPagerId);
+        setupViewPager(mViewPager);
+
+        TabLayout tabs = (TabLayout) findViewById(R.id.tabsId);
+        tabs.setupWithViewPager(mViewPager);
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        SectionsPagerAdapter adapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new UserProfileFragment(), "Profile");
+        adapter.addFragment(new UserSettingsFragment(), "Settings");
+        viewPager.setAdapter(adapter);
     }
 
     public void logout(View view) {
